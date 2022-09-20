ca777376575eb1d319ffe50cb3c7d6dfb6d3d6b7
VektorTech <kennymax18@gmail.com>
2022-09-20 15:36:05 -0500

    add route for user sign in and edit

---
+++ pages/api/auth/edit/[userId].ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs";
+
+import prisma from "@lib/prisma";
+import { withSessionRoute } from "@lib/withSession";
+import { userSchema } from "@lib/validations";
+import { User } from "@prisma/client";
+import { validateInput } from "@lib/helpers";
+import { DefaultResponse } from "src/types/shared";
+
+async function editUserRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<DefaultResponse>
+) {
+  if (req.method == "POST") {
+    const error = validateInput(req.body, userSchema);
+    if (error) {
+      return res.status(400).json({ error: true, message: error });
+    }
+
+    const { username, email, firstName, lastName, phoneNumber, avatar } =
+      userSchema.cast(req.body) as unknown as User;
+
+    const { user } = req.session;
+
+    if (user) {
+      prisma.user
+        .update({
+          where: { id: user.id },
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            phoneNumber,
+            avatar,
+          },
+        })
+        .then(() => res.json({ message: "User Update Successful" }))
+        .catch((error) =>
+          res.status(500).json({ error: true, message: error.message })
+        );
+    } else
+      res.status(401).json({ error: true, message: "Authentication Required" });
+  } else res.status(404).json({ error: true, message: "Not Found" });
+}
+
+export default withSessionRoute(editUserRoute);
---
+++ pages/api/auth/signin.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs";
+
+import prisma from "@lib/prisma";
+import { withSessionRoute } from "@lib/withSession";
+import { userLoginSchema } from "@lib/validations";
+import { User } from "@prisma/client";
+import { validateInput } from "@lib/helpers";
+import { DefaultResponse } from "src/types/shared";
+
+async function signinRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<DefaultResponse>
+) {
+  if (req.method == "POST") {
+    const error = validateInput(req.body, userLoginSchema);
+    if (error) {
+      return res.status(400).json({ error: true, message: error });
+    }
+
+    const { email, password } = userLoginSchema.cast(req.body) as User;
+
+    let user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      user = await prisma.user.findUnique({
+        where: { username: email },
+      });
+    }
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = {
+        id: user.id,
+        username: user.username,
+        role: user.role,
+      };
+
+      await req.session.save();
+
+      return res.json({ message: `${user.username}, Sign In Successful` });
+    }
+    res.status(401).json({ error: true, message: "Authentication Failed" });
+  } else res.status(404).json({ error: true, message: "Not Found" });
+}
+
+export default withSessionRoute(signinRoute);
