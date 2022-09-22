0a23ae24aefa6d364343ca353a9cf7ec496c6897
VektorTech <kennymax18@gmail.com>
2022-09-22 13:21:06 -0500

    add banner section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,26 @@ import Head from 'next/head';
 // import { useEffect } from 'react';
 import { withSessionSsr } from "@lib/withSession";
 
+import { AiOutlineMenu, AiOutlineTags } from "react-icons/ai";
 import { BsXLg, BsCart3, BsArrowRight } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdSupportAgent } from "react-icons/md";
+import {
+  RiRadioButtonLine,
+  RiCheckboxBlankCircleFill,
+  RiFacebookBoxFill,
+  RiInstagramFill,
+  RiYoutubeFill,
+  RiTwitterFill,
+  RiPinterestFill
+} from "react-icons/ri";
+
+// import prisma from '@lib/prisma';
 import Link from 'next/link';
 import Image from 'next/future/image';
+import Img from 'next/image';
 import LogoImg from '@images/jones-logo.png';
+import JordanBanner from '@images/jordan_1_banner.png';
 
 const Home: NextPage = (props) => {
   return (
@@ -67,6 +83,60 @@ const Home: NextPage = (props) => {
           </div>
         </div>
       </header>
+
+      <section className="banner">
+        <div className="banner__container">
+          <div className="banner__background"></div>
+          <div className="banner__indicators">
+            <button className="banner__indicator banner__indicator--active">
+              <RiRadioButtonLine />
+            </button>
+            <button className="banner__indicator">
+              <RiCheckboxBlankCircleFill />
+            </button>
+            <button className="banner__indicator">
+              <RiCheckboxBlankCircleFill />
+            </button>
+          </div>
+
+          <div className="banner__social_buttons">
+            <button className="banner__social-icon">
+              <RiFacebookBoxFill />
+            </button>
+            <button className="banner__social-icon">
+              <RiInstagramFill />
+            </button>
+            <button className="banner__social-icon">
+              <RiYoutubeFill />
+            </button>
+            <button className="banner__social-icon">
+              <RiTwitterFill />
+            </button>
+            <button className="banner__social-icon">
+              <RiPinterestFill />
+            </button>
+          </div>
+          <div className="banner__main">
+            <div className="banner__content">
+              <div className="banner__headings">
+                <p className="banner__sub-text">
+                  <span>verified</span> authentic sneakers
+                </p>
+                <h2 className="banner__title-type">heirloom</h2>
+                <h3 className="banner__title">retro high og</h3>
+              </div>
+              <div className="banner__image">
+                <Img layout='responsive' width={220} height={144} src={JordanBanner} />
+              </div>
+              <div className="banner__action-button">
+                <Link href="/">
+                  <a className="banner__action-button-element"><span>buy yours</span></a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
 
       <button>Hello</button>
