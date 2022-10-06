649da18834c9bf1cad7b182115cc67fb0d532f01
VektorTech <kennymax18@gmail.com>
2022-10-06 12:55:10 -0500

    define ap route for searching products

---
+++ pages/api/products/search.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@Lib/prisma";
+import { DefaultResponse } from "src/types/shared";
+
+export default async function productSearchRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<DefaultResponse>
+) {
+  	if (req.method == "GET") {
+		const { q, offset = 0, limit = 10 } = req.query;
+
+		prisma.jordanOne.findMany({
+			select: { id: true, title: true, mediaURLs: true, price: true, ratings: true, gender: true },
+			where: {
+				title: {
+					contains: q as string
+				}
+			},
+			orderBy: {
+				salesCount: 'asc'
+			},
+			skip: offset as number,
+			take: limit as number
+		})
+		.then(products => res.json({ message: "Products Found", data: products }))
+		.catch(error => res.status(500).json({ error: true, message: error.message }));
+	} else res.status(404).json({ error: true, message: "Not Found" });
+}
