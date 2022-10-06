b07a1b307493873a060e443336f1ad6fb93c4b99
VektorTech <kennymax18@gmail.com>
2022-10-06 13:00:21 -0500

    define route for admin to fetch multiple users

---
+++ pages/api/auth/index.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@Lib/prisma";
+import { withSessionRoute } from "@Lib/withSession";
+import { Role } from "@prisma/client";
+import { DefaultResponse } from "src/types/shared";
+
+async function usersRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<DefaultResponse>
+) {
+  if (req.method == "GET") {
+	const { user } = req.session;
+	const { offset = 0, limit = 10 } = req.query;
+	const isAuthorized = user?.role == Role.ADMIN;
+
+    if (isAuthorized) {
+	  prisma.user.findMany({
+		skip: offset as number,
+		take: limit as number,
+		select: {
+			id: true,
+			avatar: true,
+			username: true,
+			email: true,
+			firstName: true,
+			lastName: true,
+			phoneNumber: true,
+			deactivated: true
+		},
+	  })
+	  .then(users => {
+		res.json({ message: "Successfully Retrieved Users", data: users });
+ 	  })
+	  .catch(error => {
+		res.status(500).json({ message: error.message });
+	  });
+    }
+    res.status(401).json({ error: true, message: "Unauthorized Request" });
+  } else res.status(404).json({ error: true, message: "Not Found" });
+}
+
+export default withSessionRoute(usersRoute);
