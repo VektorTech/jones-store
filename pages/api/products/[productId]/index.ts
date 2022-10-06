fa9e80ad156c8abefc1d59de9c7e1ca4212806e7
VektorTech <kennymax18@gmail.com>
2022-10-06 12:54:49 -0500

    define api route for fetching products

---
+++ pages/api/products/[productId]/index.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@Lib/prisma";
+import { DefaultResponse } from "src/types/shared";
+
+export default async function productRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<DefaultResponse>
+) {
+  	if (req.method == "GET") {
+		const { productId } = req.query;
+
+		prisma.jordanOne.findUnique({
+			where: {
+				id: productId as string
+			}
+		})
+		.then(product => res.json({ message: "Product Found", data: product }))
+		.catch(error => res.status(500).json({ error: true, message: error.message }));
+	} else res.status(404).json({ error: true, message: "Not Found" });
+}
