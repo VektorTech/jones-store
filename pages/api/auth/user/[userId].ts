0154159775283546769b22e4b969fed340e0c3ca
VektorTech <kennymax18@gmail.com>
2022-10-06 12:56:46 -0500

    define api route for retrieving a user record

---
+++ pages/api/auth/user/[userId].ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@Lib/prisma";
+import { withSessionRoute } from "@Lib/withSession";
+import { Role } from "@prisma/client";
+import { DefaultResponse } from "src/types/shared";
+
+async function userRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<DefaultResponse>
+) {
+  if (req.method == "GET") {
+	const { userId } = req.query;
+	const { user } = req.session;
+
+	const isAuthorized = userId && user && (userId == user?.id || user?.role == Role.ADMIN);
+
+    if (isAuthorized) {
+	  prisma.user.findUnique({
+		select: {
+			id: true,
+			avatar: true,
+			username: true,
+			email: true,
+			firstName: true,
+			lastName: true,
+			phoneNumber: true,
+			deactivated: true
+		},
+		where: { id: userId as string },
+	  })
+	  .then(userData => {
+		res.json({ message: "Successfully Retrieved User Record", data: userData });
+ 	  })
+	  .catch(error => {
+		res.status(500).json({ message: error.message });
+	  });
+    }
+    res.status(401).json({ error: true, message: "Unauthorized Request" });
+  } else res.status(404).json({ error: true, message: "Not Found" });
+}
+
+export default withSessionRoute(userRoute);
