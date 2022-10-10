aed161fd2c53149f0acfa607e774bb27f2ee4860
VektorTech <kennymax18@gmail.com>
2022-10-09 23:52:29 -0500

    create context for managing UI state

---
+++ src/lib/contexts/UIContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, Dispatch, ReactElement, SetStateAction, useContext, useEffect, useState } from "react";
+
+export const Dialogs = {
+	SIDEBAR_DIALOG: "SIDEBAR_DIALOG",
+	MODAL_POPUP: "MODAL_POPUP"
+};
+
+export type DialogStates = keyof typeof Dialogs | null;
+
+const uiState: {
+	announcementVisible?: boolean;
+	currentDialog?: DialogStates;
+	setDialog: Dispatch<SetStateAction<DialogStates>>;
+} = {
+	announcementVisible: true,
+	currentDialog: undefined,
+	setDialog: () => {}
+};
+
+const UIContext = createContext(uiState);
+
+export function useDialog(observer?: (isVisible: boolean) => void, dialog?: DialogStates) {
+	const _uiState = useContext(UIContext);
+
+	useEffect(() => {
+	  observer?.(_uiState.currentDialog == (dialog || Dialogs.SIDEBAR_DIALOG));
+	}, [_uiState, observer, dialog]);
+
+	return _uiState;
+}
+
+export function useAnnouncementState() {
+	const { announcementVisible } = useContext(UIContext);
+	return announcementVisible;
+}
+
+export const UIProvider = ({ children, announcementState = true }: { children: ReactElement, announcementState: boolean }) => {
+	const [currentDialog, setDialog] = useState<DialogStates>(null);
+
+	return (
+	  <UIContext.Provider value={{ announcementVisible: announcementState, currentDialog, setDialog }}>
+		{children}
+	  </UIContext.Provider>
+	);
+};
