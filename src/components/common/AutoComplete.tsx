c0776d45162cd330302850497249147198d851c4
VektorTech <kennymax18@gmail.com>
2022-10-13 01:43:48 -0500

    add autocomplete component

---
+++ src/components/common/AutoComplete.tsx
@@ -0,0 +1,15 @@
+export default function AutoComplete({
+	label,
+	className,
+	...inputProps
+  }: { label?: string } & JSX.IntrinsicElements["input"]) {
+	return (
+	  <div className={`auto-complete${className ? " " + className : ""}`}>
+		<label className="text-input__label">
+		  <span className="text-input__label-text">{label}</span>
+		  <input className="text-input__control" {...inputProps} />
+		</label>
+
+	  </div>
+	);
+  }
--- src/components/common/TextField.tsx
+++ src/components/common/TextField.tsx
@@ -1,13 +1,20 @@
 export default function TextField({
   label,
   className,
+  multiline = false,
+  name = "",
   ...inputProps
-}: { label: string; className?: string } & JSX.IntrinsicElements["input"]) {
+}: { label: string; multiline?:boolean; name?: string; className?: string; } & JSX.IntrinsicElements["input"]) {
   return (
     <div className={`text-input${className ? " " + className : ""}`}>
       <label className="text-input__label">
         <span className="text-input__label-text">{label}</span>
-        <input className="text-input__control" {...inputProps} />
+        {
+          multiline ?
+          <textarea name={name} className="text-input__control"></textarea>
+          :
+          <input className="text-input__control" {...inputProps} />
+        }
       </label>
     </div>
   );
