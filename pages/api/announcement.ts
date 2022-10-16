ef0e0a2b2f16cb5384fa9e8fa877de55950240df
VektorTech <kennymax18@gmail.com>
2022-10-16 01:41:23 -0500

    rename api middleware

--- pages/api/announcement.ts
+++ pages/api/announcement.ts
@@ -3,7 +3,7 @@ import prisma from "@Lib/prisma";
 import { DefaultResponse } from "src/types/shared";
 import sanitizeHtml from 'sanitize-html';
 import { RouteHandler } from "@Lib/RouteHandler";
-import { authorizeRole, isAuthenticated } from "@Lib/apiMiddlewares";
+import { authorizeRole, isAuthenticated } from "@Lib/apiMiddleware";
 
 export default new RouteHandler()
 	.post(isAuthenticated, authorizeRole("ADMIN"), async function AnnouncementPostRoute(req: NextApiRequest, res: NextApiResponse<DefaultResponse>, next: Function) {
