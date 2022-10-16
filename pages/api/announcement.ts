405432f5035afacbcce439ff88d8ea31ec647ab9
VektorTech <kennymax18@gmail.com>
2022-10-16 01:39:45 -0500

    use method route handler

--- pages/api/announcement.ts
+++ pages/api/announcement.ts
@@ -1,20 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
 import prisma from "@Lib/prisma";
 import { DefaultResponse } from "src/types/shared";
-import { withSessionRoute } from "@Lib/withSession";
-import { Role } from "@prisma/client";
 import sanitizeHtml from 'sanitize-html';
+import { RouteHandler } from "@Lib/RouteHandler";
+import { authorizeRole, isAuthenticated } from "@Lib/apiMiddlewares";
 
-async function AnnouncementRoute(
-  req: NextApiRequest,
-  res: NextApiResponse<DefaultResponse>
-) {
-  if (req.method == "POST") {
-	const { headline, details } = req.body;
-	const { user } = req.session;
+export default new RouteHandler()
+	.post(isAuthenticated, authorizeRole("ADMIN"), async function AnnouncementPostRoute(req: NextApiRequest, res: NextApiResponse<DefaultResponse>, next: Function) {
+		const { headline, details } = req.body;
 
-	if (user && user.role == Role.ADMIN) {
 		const allowedTags = [ 'b', 'i', 'em', 'strong', 'span', 'sub', 'sup', 'a'];
 
 		await prisma.announcement.create({
@@ -22,33 +16,21 @@ async function AnnouncementRoute(
 				headline: sanitizeHtml(headline, { allowedTags }),
 				details: sanitizeHtml(details, { allowedTags })
 			}
-		})
-		.then(() =>
-			res.status(201).json({
-				message: "Successfully Created Announcement",
-			})
-		)
-		.catch((error) =>
-			res.status(500).json({ error: true, message: error.message })
-		);
-	}
-  } else if (req.method == "GET") {
-	await prisma.announcement
-		.findMany({
-			orderBy: { addedAt: "desc" }
-		})
-		.then((announcement) =>
-			res.json({
-				message: "Successfully Retrieved Announcement(s)",
-				data: announcement,
-			})
-		)
-		.catch((error) =>
-			res.status(500).json({ error: true, message: error.message })
-		);
-  } else {
-    res.status(404).json({ error: true, message: "Not Found" });
-  }
-}
+		});
 
-export default withSessionRoute(AnnouncementRoute);
+		res.status(201).json({
+			message: "Successfully Created Announcement",
+		});
+	})
+	.get(async function AnnouncementGetRoute(req: NextApiRequest, res: NextApiResponse<DefaultResponse>, next: Function) {
+		const announcement = await prisma.announcement
+			.findMany({
+				orderBy: { addedAt: "desc" }
+			});
+
+		res.json({
+			message: "Successfully Retrieved Announcement(s)",
+			data: announcement,
+		});
+	})
+	.init();
