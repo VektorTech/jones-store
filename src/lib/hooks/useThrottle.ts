9cec3bd79a1e524c2d0b3e2259809129a6246d08
VektorTech <kennymax18@gmail.com>
2022-10-16 23:45:11 -0500

    create custom throttle hook

---
+++ src/lib/hooks/useThrottle.ts
@@ -0,0 +1,9 @@
+import { useEffect, DependencyList } from "react";
+
+export const useThrottle = (callback: Function, delay: number, deps: DependencyList) => {
+	useEffect(() => {
+		const timerID = setTimeout(callback, delay);
+		return () => clearTimeout(timerID);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, deps);
+};
