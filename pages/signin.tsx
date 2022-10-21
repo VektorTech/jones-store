b6aaf39a849548f5c5049f72d5246968e5decb80
VektorTech <kennymax18@gmail.com>
2022-10-21 15:55:21 -0500

    remove test usage

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,9 +1,3 @@
-import AutoComplete from "@Components/common/formControls/AutoComplete";
-import Button from "@Components/common/formControls/Button";
-import Dropdown from "@Components/common/formControls/Dropdown";
-import RadioList from "@Components/common/formControls/RadioList";
-import TextField from "@Components/common/formControls/TextField";
-
 import SEO from "@Components/common/SEO";
 import AuthForm from "@Components/user/AuthForm";
 import HeroBanner from "@Components/HeroBanner";
@@ -14,36 +8,6 @@ export default function SignInPage() {
       <HeroBanner short />
       <SEO title="Sign In" />
       <AuthForm />
-
-      <br />
-
-      <div style={{ width: "50%", margin: "0 auto" }}>
-        <TextField name="email" type="email" required label="Email" />
-        <TextField multiline label="Full Name" />
-        <TextField type="password" label="Password" />
-        <Button>Test Action</Button>
-        <Dropdown options={["red", "blue", "green", "indigo", "violet"]} />
-        <AutoComplete options={["red", "blue", "green", "indigo", "violet"]} />
-        <RadioList
-          label="Select Colors"
-          name="color"
-          values={["red", "blue", "green", "indigo", "violet"]}
-        />
-        <RadioList
-          label="Select One Fruit"
-          name="fruit"
-          checkbox
-          values={[
-            "apple",
-            "banana",
-            "grape",
-            "mango",
-            "melon",
-            "orange",
-            "pineapple",
-          ]}
-        />
-      </div>
     </>
   );
 }
