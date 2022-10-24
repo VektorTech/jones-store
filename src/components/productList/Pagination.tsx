7b11f605b97c22969a5c2bbabd2a3ee0bc0ac098
VektorTech <kennymax18@gmail.com>
2022-10-23 19:45:18 -0500

    style pagination component

--- src/components/productList/Pagination.tsx
+++ src/components/productList/Pagination.tsx
@@ -1,16 +1,27 @@
-import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
+import { AiOutlineDoubleLeft, AiOutlineDoubleRight, AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 export default function Pagination() {
   return (
     <div className="results__pagination">
-      <button className="results__pagination-first">
+      <button className="results__pagination-button results__pagination-button--disabled">
         <AiOutlineDoubleLeft />
       </button>
-      <button className="results__pagination-prev">Prev</button>
-      <button className="results__pagination-page">1</button>
-      <button className="results__pagination-page">2</button>
-      <button className="results__pagination-next">Next</button>
-      <button className="results__pagination-last">
+      <button className="results__pagination-button results__pagination-button--disabled">
+        <AiOutlineLeft />
+      </button>
+      <button className="results__pagination-button results__pagination-button--active">
+        1
+      </button>
+      <button className="results__pagination-button">
+        2
+      </button>
+      <button className="results__pagination-button">
+        3
+      </button>
+      <span className="results__pagination-button">&#8230;</span>
+      <button className="results__pagination-button">9</button>
+      <button className="results__pagination-button"><AiOutlineRight /></button>
+      <button className="results__pagination-button">
         <AiOutlineDoubleRight />
       </button>
     </div>
