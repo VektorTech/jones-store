f86f215084bbb7d82c2eb131f875f85928fe9a67
VektorTech <kennymax18@gmail.com>
2022-10-23 19:43:49 -0500

    style breadcrumb list

--- src/components/productList/BreadCrumbs.tsx
+++ src/components/productList/BreadCrumbs.tsx
@@ -7,19 +7,19 @@ export default function BreadCrumbs() {
         <ul className="breadcrumb__list">
           <li className="breadcrumb__list-item">
             <Link href="">
-              <a>Home</a>
+              <a className="breadcrumb__link breadcrumb__link--root">Home</a>
             </Link>
           </li>
-          <li className="breadcrumb__list-sep"> | </li>
+          <li className="breadcrumb__list-item">〉</li>
           <li className="breadcrumb__list-item">
             <Link href="">
-              <a>Women</a>
+              <a className="breadcrumb__link">Women</a>
             </Link>
           </li>
-          <li className="breadcrumb__list-sep"> | </li>
+          <li className="breadcrumb__list-item">〉</li>
           <li className="breadcrumb__list-item">
             <Link href="">
-              <a>Mids</a>
+              <a className="breadcrumb__link">Mids</a>
             </Link>
           </li>
         </ul>
