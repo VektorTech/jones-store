0fd118a2fb0890689023476000860c53f6065981
VektorTech <kennymax18@gmail.com>
2022-10-23 19:51:05 -0500

    build price range input

--- src/components/productList/Filter.tsx
+++ src/components/productList/Filter.tsx
@@ -10,19 +10,141 @@ export default function Filter() {
         </button>
       </div>
 
-      <div className="filter__param">
-        <label className="filter__param-section">
-          <span className="filter__param-type">
-            <IoIosArrowUp />
-          </span>
-          <input type="checkbox" className="filter__param-hide" />
-          <div className="filter__param-body"></div>
-        </label>
-      </div>
 
-      <div className="filter__confirm">
-        <button className="filter__clear-all">clear all</button>
-        <button className="filter__done">done</button>
+const PriceRange = () => {
+  const HIGHEST_PRICE = 1000;
+  const [valueMin, setValMin] = useState("0");
+  const [valueMax, setValMax] = useState(HIGHEST_PRICE.toString());
+
+  const [activeThumb, setActiveThumb] = useState("");
+  const minRef = useRef<HTMLSpanElement>(null);
+  const maxRef = useRef<HTMLSpanElement>(null);
+  const rangeRef = useRef<HTMLSpanElement>(null);
+  const controlRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const mouseUpHandler = (e: MouseEvent) => {
+      setActiveThumb("");
+    };
+
+    const asPercentage = (num: number) =>
+      (num / Number(controlRef.current?.offsetWidth)) * 100;
+
+    const mouseMoveHandler = ({ clientX }: MouseEvent) => {
+      const minThumb = minRef.current;
+      const maxThumb = maxRef.current;
+      const rangeTrack = rangeRef.current;
+      const control = controlRef.current;
+
+      if (!(minThumb && maxThumb && control && rangeTrack)) {
+        return;
+      }
+
+      const pageX = clientX - control.offsetLeft;
+      const limit = control.offsetWidth - maxThumb.offsetWidth * 2;
+
+      if (activeThumb == "min") {
+        const thumbLeft = asPercentage(
+          Math.max(
+            0,
+            Math.min(pageX - minThumb.offsetWidth / 2, maxThumb.offsetLeft)
+          )
+        );
+        minThumb.style.left = thumbLeft + "%";
+        minThumb.classList.toggle(
+          "price-range__thumb--above",
+          thumbLeft >= asPercentage(limit)
+        );
+        setValMin(
+          Number(
+            (minThumb.offsetLeft /
+              (control.offsetWidth - minThumb.offsetWidth)) *
+              HIGHEST_PRICE
+          ).toFixed(0)
+        );
+      } else if (activeThumb == "max") {
+        const thumbLeft = asPercentage(
+          Math.min(
+            Math.max(
+              minThumb.offsetLeft - minThumb.offsetWidth,
+              pageX - maxThumb.offsetWidth * 1.5
+            ),
+            limit
+          )
+        );
+        maxThumb.style.left = thumbLeft + "%";
+        setValMax(
+          Number(
+            (maxThumb.offsetLeft /
+              (control.offsetWidth - maxThumb.offsetWidth)) *
+              HIGHEST_PRICE
+          ).toFixed(0)
+        );
+      }
+
+      rangeTrack.style.left = asPercentage(minThumb.offsetLeft) + "%";
+      rangeTrack.style.width =
+        asPercentage(maxThumb.offsetLeft - minThumb.offsetLeft) + "%";
+    };
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      if (rangeRef.current && minRef.current && maxRef.current) {
+        rangeRef.current.style.left =
+          asPercentage(minRef.current.offsetLeft) + "%";
+        rangeRef.current.style.width =
+          asPercentage(maxRef.current.offsetLeft - minRef.current.offsetLeft) +
+          "%";
+      }
+    });
+
+    if (controlRef.current) {
+      resizeObserver.observe(controlRef.current);
+    }
+
+    if (activeThumb) {
+      document.addEventListener("mouseup", mouseUpHandler);
+      document.addEventListener("mousemove", mouseMoveHandler);
+    }
+
+    return () => {
+      document.removeEventListener("mouseup", mouseUpHandler);
+      document.removeEventListener("mousemove", mouseMoveHandler);
+      resizeObserver.disconnect();
+    };
+  }, [activeThumb]);
+
+  return (
+    <div className="price-range">
+      <input
+        defaultValue={`$${valueMin} - $${valueMax}`}
+        readOnly
+        name="price-range"
+        className="price-range__input"
+      />
+      <div ref={controlRef} className="price-range__control">
+        <span className="price-range__range"></span>
+        <span ref={rangeRef} className="price-range__bar"></span>
+        <span
+          ref={minRef}
+          onMouseDown={(e) => {
+            e.preventDefault(); // prevent selection
+            setActiveThumb("min");
+          }}
+          className="price-range__min"
+        ></span>
+        <span
+          ref={maxRef}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            setActiveThumb("max");
+          }}
+          className="price-range__max"
+        ></span>
+      </div>
+    </div>
+  );
+};
+
       </div>
     </div>
   );
