30f281db5b541bc9651cf0080d432f9e6be6cd17
VektorTech <kennymax18@gmail.com>
2022-10-25 23:23:36 -0500

    style 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Custom404() {
   return (
-    <h1>
-      <em>404</em> &mdash; Page Not Found!
-    </h1>
+    <div className="not-found">
+      <h1 className="not-found__title">
+        <span>404</span>
+      </h1>
+      <h2 className="not-found__sub-title">Page Not Found!</h2>
+      <p className="not-found__text">
+        The page you were looking for does not exist.
+      </p>
+      <Link href={"/"}>
+        <a className="not-found__link">Homepage</a>
+      </Link>
+    </div>
   );
 }
