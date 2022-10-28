8e9ae0f97f3ef3049199bccb05db1a573633267c
VektorTech <kennymax18@gmail.com>
2022-10-28 01:25:40 -0500

    add api route for reviews, post and get

---
+++ pages/api/products/[productId]/review.ts
@@ -0,0 +1,56 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import prisma from "@Lib/prisma";
+import { DefaultResponse } from "src/types/shared";
+import { RouteHandler } from "@Lib/RouteHandler";
+import { isAuthenticated } from "@Lib/apiMiddleware";
+import { ServerError } from "@Lib/utils";
+
+async function postReviewRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<DefaultResponse>,
+  next: Function
+) {
+  const { productId } = req.query;
+  const { user } = req.session;
+  const { rating = 0, body = "" } = req.body;
+
+  if (typeof productId == "string") {
+    const review = await prisma.review.create({
+      data: {
+        rating: Number(rating),
+        comment: body,
+        productId,
+        userId: user?.id as string,
+      },
+    });
+
+    return res.status(201).json({ message: "Review Created", data: review });
+  }
+  next(new ServerError("Malformed Request", 400));
+}
+
+async function getReviewsRoute(
+  req: NextApiRequest,
+  res: NextApiResponse<DefaultResponse>,
+  next: Function
+) {
+  const { productId } = req.query;
+
+  if (typeof productId == "string") {
+    const reviews = await prisma.review.findMany({
+      where: { productId },
+      include: { user: { select: { username: true, avatarURL: true } } },
+    });
+    return res.json({
+      message: reviews.length + " Reviews Found",
+      data: reviews,
+    });
+  }
+  next(new ServerError("Malformed Request", 400));
+}
+
+export default new RouteHandler()
+  .post(isAuthenticated, postReviewRoute)
+  .get(getReviewsRoute)
+  .init();
