00f605ac409d582d0af569f0a935230aefe0baa3
VektorTech <kennymax18@gmail.com>
2022-10-29 22:43:12 -0500

    add sort and product results to search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,10 +4,64 @@ import prisma from "@Lib/prisma";
 import { withSessionSsr } from "@Lib/withSession";
 import SEO from "@Components/common/SEO";
 
-const SearchPage: NextPage<{ query: string; products?: any }> = ({ query }) => {
+import { Product as ProductType } from "@prisma/client";
+import Product from "@Components/common/Product";
+import Pagination from "@Components/productList/Pagination";
+import { useRouter } from "next/router";
+import Dropdown from "@Components/common/formControls/Dropdown";
+
+const RESULTS_PER_PAGE = 20;
+
+const SearchPage: NextPage<{
+  query: string;
+  products: ProductType[];
+  count: number;
+}> = ({ query, products, count }) => {
+  const router = useRouter();
+  const { offset = 0 } = router.query;
+
   return (
     <div>
       <SEO title={`"${query}"`} />
+
+      <div className="filter-sort">
+        <div className="filter-sort__container">
+          <div className="filter-sort__sort-by">
+            <Dropdown
+              label="Sort By"
+              className="filter-sort__sort-select"
+              options={{
+                relevance: "Relevance",
+                asc_price: "Asc: Price",
+                price: "Desc: Price",
+                asc_ratings: "Asc: Ratings",
+                ratings: "Desc: Ratings",
+              }}
+              onOptionSelect={(order) => {
+                if (order) {
+                  router.query.order = order;
+                  router.push(router);
+                }
+              }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="results">
+        <div className={"results__container"}>
+          <div className="results__grid">
+            {products.map((product) => (
+              <Product key={product.id} {...product} />
+            ))}
+          </div>
+          <Pagination
+            resultsCount={count}
+            limit={RESULTS_PER_PAGE}
+            offset={Number(offset)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
@@ -17,7 +71,7 @@ export const getServerSideProps = withSessionSsr(async function ({
   req,
   query,
 }) {
-  const { q = "", offset = 0, limit = 10 } = query;
+  const { search = "", offset = 0, limit = RESULTS_PER_PAGE, order, } = query;
 
   const productColumns = {
     title: true,
@@ -30,18 +84,35 @@ export const getServerSideProps = withSessionSsr(async function ({
     id: true,
   };
 
+  const orderBy: { orderBy: any } = { orderBy: {} };
+  if (order && typeof order == "string") {
+    const orderings: { [order: string]: any } = {
+      asc_price: { price: "asc" },
+      price: { price: "desc" },
+      asc_ratings: { ratings: "asc" },
+      ratings: { ratings: "desc" },
+    };
+    orderBy["orderBy"] = { ...orderings[order] };
+  }
+
   const results =
     (await prisma.product.findMany({
       select: productColumns,
-      where: { title: { contains: q as string } },
+      where: { title: { contains: search as string, mode: "insensitive" } },
       skip: Number(offset),
       take: Number(limit),
+      ...orderBy
     })) || null;
+
+  let count = await prisma.product.count({
+    where: { title: { contains: search as string, mode: "insensitive" } },
+  });
 
   return {
     props: {
-      query: q,
+      query: search,
       products: results,
+      count,
     },
   };
 });
