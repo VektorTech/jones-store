3e799b291e8384bc612435f0cc6b8eb3e6ae40b9
VektorTech <kennymax18@gmail.com>
2022-10-29 20:58:31 -0500

    add toggle to auth form

--- src/components/user/AuthForm.tsx
+++ src/components/user/AuthForm.tsx
@@ -1,6 +1,28 @@
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 
+import { useState } from "react";
+
 export default function AuthForm({ isNewUser = false }) {
-  return <div>{isNewUser ? <SignUp /> : <SignIn />}</div>;
+  const [signUp, setSignUp] = useState(isNewUser);
+
+  return (
+    <div className="auth">
+      <div className={"auth__container " + (signUp ? "auth__container--signup" : "auth__container--signin")}>
+        <div className={"auth__form" + (signUp ? " auth__form--active" : "")}>
+          <SignUp />
+          <button className="auth__button" onClick={() => setSignUp(false)}>
+            Already have an account?
+          </button>
+        </div>
+
+        <div className={"auth__form" + (!signUp ? " auth__form--active" : "")}>
+          <SignIn />
+          <button className="auth__button" onClick={() => setSignUp(true)}>
+            Don&apos;t have an account?
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
