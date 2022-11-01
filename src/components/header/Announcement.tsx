22dc2fd27f76caa549cb1863b4873ee556385515
VektorTech <kennymax18@gmail.com>
2022-11-01 01:28:59 -0500

    make announcement component marquee content

--- src/components/header/Announcement.tsx
+++ src/components/header/Announcement.tsx
@@ -5,12 +5,15 @@ import type { Announcement as AnnouncementType } from "@prisma/client";
 import { useEffect, useState } from "react";
 import { BsXLg } from "react-icons/bs";
 import { FiHelpCircle } from "react-icons/fi";
+import { ClipLoader } from "react-spinners";
 
 export default function Announcement() {
   const [announcementVisible, setAnnouncementVisible] = useAnnouncementState();
-  const [content, setContent] = useState<AnnouncementType>();
+  const [content, setContent] = useState<AnnouncementType[]>([]);
+  const [loading, setLoading] = useState(true);
 
   const { currentDialog, setDialog } = useDialog();
+  const [currentDetails, setCurrentDetails] = useState("");
   const visible = currentDialog == "MODAL_POPUP";
 
   const handleClose = () => {
@@ -18,16 +21,41 @@ export default function Announcement() {
     setCookie("announcementState", "closed", 14);
   };
 
+  const announcementBody = content.map(({ headline, details }) => (
+    <div
+      key={headline}
+      className="announcement__inner"
+      onClickCapture={(e) => {
+        e.preventDefault();
+        setCurrentDetails(details);
+      }}
+    >
+      <span dangerouslySetInnerHTML={{ __html: headline || "" }}></span>
+      <FiHelpCircle className="announcement__info-icon" />
+    </div>
+  ));
+
+  useEffect(() => {
+    if (currentDetails) {
+      setDialog("MODAL_POPUP");
+    } else {
+      setDialog(null);
+    }
+  }, [currentDetails, setDialog]);
+
   useEffect(() => {
     if (announcementVisible) {
       fetch("/api/announcement")
         .then((res) => res.json())
-        .then(({ data }: { data: AnnouncementType[] }) => {
-          setContent(data[0]);
-        })
-        .catch(console.log);
+        .then(({ data }: { data: AnnouncementType[] }) => setContent(data))
+        .catch()
+        .finally(() => setLoading(false));
     }
   }, [announcementVisible]);
+
+  if ((!content || !content.length) && !loading) {
+    return null;
+  }
 
   return (
     <div
@@ -36,30 +64,28 @@ export default function Announcement() {
       }`}
     >
       <div className="announcement__container">
-        <div
-          onClickCapture={(e) => {
-            e.preventDefault();
-            setDialog("MODAL_POPUP");
-          }}
-          className="announcement__content"
-        >
-          <span
-            dangerouslySetInnerHTML={{ __html: content?.headline || "" }}
-          ></span>
-          {content ? (
-            <FiHelpCircle className="announcement__info-icon" />
-          ) : (
-            "Loading..."
-          )}
-        </div>
-        <Modal onClose={() => setDialog(null)} visible={visible}>
+        {content.length ? (
           <div
-            dangerouslySetInnerHTML={{ __html: content?.details || "" }}
-          ></div>
-        </Modal>
+            style={{ width: `${content.length * 100}%` }}
+            className="announcement__content"
+          >
+            {[...Array(2)].map((_, i) => (
+              <div key={i} className="announcement__marquee">
+                {announcementBody}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <ClipLoader className="announcement__loader" />
+        )}
+
         <button onClick={handleClose} className="announcement__close">
           <BsXLg className="announcement__close-icon" />
         </button>
+
+        <Modal onClose={() => setCurrentDetails("")} visible={visible}>
+          <div dangerouslySetInnerHTML={{ __html: currentDetails }}></div>
+        </Modal>
       </div>
     </div>
   );
