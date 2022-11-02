ef0857be29f68b0c7437367a2981e72c75c57764
VektorTech <kennymax18@gmail.com>
2022-11-02 15:29:44 -0500

    mask image over "404" text

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import BannerImage from "@Images/banner-bg-eindhoven.jpg";
 
 export default function Custom404() {
   return (
     <div className="not-found">
       <h1 className="not-found__title">
         <span>404</span>
+        <Image className="not-found__image" objectFit="cover" src={BannerImage} alt="" layout="fill" />
       </h1>
       <h2 className="not-found__sub-title">Page Not Found!</h2>
       <p className="not-found__text">
