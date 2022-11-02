ad247505b02c45f00b95420b0da3c212bdce8587
VektorTech <kennymax18@gmail.com>
2022-11-02 02:34:42 -0500

    hide review add button from un-auth users

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -6,10 +6,14 @@ import Form from "./Form";
 import Modal from "./Modal";
 import RatingStars from "./common/RatingStars";
 import Review from "./Review";
+import { useAuthState } from "@Lib/contexts/AuthContext";
+import Link from "next/link";
 
 export default function Reviews({ productId }: { productId: string }) {
   const [reviewModal, setReviewModal] = useState(false);
   const [reviews, setReviews] = useState<(ReviewType & { user: User })[]>([]);
+
+  const { user } = useAuthState();
 
   useEffect(() => {
     fetch(`/api/products/${productId}/review`)
@@ -23,30 +27,42 @@ export default function Reviews({ productId }: { productId: string }) {
 
   return (
     <div className="product-view__details-panel product-view__reviews-panel">
-      <button onClick={() => setReviewModal(true)}>Write A Review</button>{" "}
-      <Modal
-        title="Write Review"
-        visible={reviewModal}
-        onClose={() => setReviewModal(false)}
-      >
-        <div>
-          <Form
-            method="POST"
-            action={`/api/products/${productId}/review`}
-            afterSubmit={(data, status) => {
-              console.log(data, status);
-            }}
+      {user?.isAuth ? (
+        <>
+          <button onClick={() => setReviewModal(true)}>Write A Review</button>{" "}
+          <Modal
+            title="Write Review"
+            visible={reviewModal}
+            onClose={() => setReviewModal(false)}
           >
-            <RatingStars interactive />
-            <TextField name="body" multiline label="Your review" />
-            <Button>Submit Review</Button>
-          </Form>
-        </div>
-      </Modal>
+            <div>
+              <Form
+                method="POST"
+                action={`/api/products/${productId}/review`}
+                afterSubmit={(data, status) => {
+                  console.log(data, status);
+                }}
+              >
+                <RatingStars interactive />
+                <TextField name="body" multiline label="Your review" />
+                <Button>Submit Review</Button>
+              </Form>
+            </div>
+          </Modal>{" "}
+        </>
+      ) : (
+        <p>
+          Please{" "}
+          <Link href="/login">
+            <a>login</a>
+          </Link>{" "}
+          to submit a review.
+        </p>
+      )}
       <div className="reviews">
         <div className="reviews__avg-ratings">
-          Average Rating: <RatingStars count={averageRatings} /> {averageRatings.toFixed(1)} (
-          {reviews.length} Customer Reviews)
+          Average Rating: <RatingStars count={averageRatings} />{" "}
+          {averageRatings.toFixed(1)} ({reviews.length} Customer Reviews)
         </div>
         <ul className="reviews__list">
           {reviews.map((review) => (
