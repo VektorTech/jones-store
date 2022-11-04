42bb046ee3873f81dae9897a7f9b566329c79047
VektorTech <kennymax18@gmail.com>
2022-11-04 03:39:10 -0500

    change mask image for text

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import BannerImage from "@Images/banner-bg-eindhoven.jpg";
+import MaskImage from "@Images/forma_nike_03-1-2560x1660.png";
 
 export default function Custom404() {
   return (
     <div className="not-found">
       <h1 className="not-found__title">
         <span>404</span>
-        <Image className="not-found__image" objectFit="cover" src={BannerImage} alt="" layout="fill" />
+        <Image className="not-found__image" objectFit="cover" src={MaskImage} alt="" layout="fill" />
       </h1>
       <h2 className="not-found__sub-title">Page Not Found!</h2>
       <p className="not-found__text">
