c83066ca19c3c75e624579ce4b3021c8a716478c
VektorTech <kennymax18@gmail.com>
2022-11-07 14:58:45 -0500

    remove old code

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,9 +1,1 @@
-import Button from "@Components/common/formControls/Button";
-
-export default function AdminDashboard() {
-  return (
-    <div className="admin">
-      <Button>Hello World</Button>
-    </div>
-  );
-}
+export {}
