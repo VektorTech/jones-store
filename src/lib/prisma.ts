545a6773d0afb0af5af67d136063ff09b31c977f
VektorTech <kennymax18@gmail.com>
2022-11-20 21:39:01 -0500

    refactor

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,8 +3,12 @@ import { PrismaClient } from "@prisma/client";
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production" && typeof window == "undefined") {
-  prisma = global.prisma || new PrismaClient();
-  global.prisma = prisma;
+  if (global.prisma) {
+    prisma = global.prisma;
+  } else {
+    prisma = new PrismaClient();
+    global.prisma = prisma;
+  }
 } else {
   if (!global.prisma && typeof window == "undefined") {
     global.prisma = new PrismaClient();
