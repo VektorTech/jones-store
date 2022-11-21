4c7d00198f4ed558cea508d9ee83b8e8fc3b1e36
VektorTech <kennymax18@gmail.com>
2022-11-20 20:31:19 -0500

    ensure to assign instance to global

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,7 @@ let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production" && typeof window == "undefined") {
   prisma = global.prisma || new PrismaClient();
+  global.prisma = prisma;
 } else {
   if (!global.prisma && typeof window == "undefined") {
     global.prisma = new PrismaClient();
