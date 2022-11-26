d4e26bb65e1880986cebb596e83e50dce9dbb282
VektorTech <kennymax18@gmail.com>
2022-11-26 02:40:54 -0500

    remove 300 font-weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,600;0,700;0,900;1,300;1,400;1,600;1,700;1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,600;0,700;0,900;1,400;1,600;1,700;1,900&display=swap"
           rel="stylesheet"
         ></link>
 
