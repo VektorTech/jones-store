0252481e32d1c4040f7ed8a47bf532c990762172
VektorTech <kennymax18@gmail.com>
2023-02-16 04:48:14 -0500

    refactor prisma instantiation code

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const prisma: PrismaClient =
+  global.prisma || (typeof window == "undefined" && new PrismaClient());
 
-if (process.env.NODE_ENV === "production" && typeof window == "undefined") {
-  prisma = global.prisma || new PrismaClient();
-} else {
-  if (!global.prisma && typeof window == "undefined") {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+if (process.env.NODE_ENV === "production") {
+  global.prisma = prisma;
 }
 
 export default prisma;
